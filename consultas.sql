e66a6602eb58a362fb00a8bf6e4f4a1fba5c9d52
andrebariani <andrebariani@gmail.com>
2019-01-20 11:51:33 -0200

    consultas.sql v2

--- consultas.sql
+++ consultas.sql
@@ -2,10 +2,16 @@
 SELECT cod_lote AS Codigo, data_vencimento AS Vence_em FROM Ingrediente, Lote WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = '18';
 
 --Recuperar a quantidade total de um ingrediente
-SELECT SUM(quantidade_lote) FROM Lote, Ingrediente WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = '1';
+SELECT Ingrediente.cod_ingrediente, SUM(quantidade_lote) FROM Lote, Ingrediente WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = '1';
 
---Recuperar a media dos preços de todos os pratos
-SELECT AVG(preco_prato) FROM Prato;
+--Recuperar a media dos preços dos pratos que cada cozinheiro faz
+SELECT nome_cozinheiro, AVG(preco_prato) AS media_preco_prato FROM Prato, Cozinheiro GROUP BY nome_cozinheiro;
 
 --Recuperar Quais pratos determinado cozinheiro faz
-SELECT nome_prato FROM Prato Where Prato.CPF = '165.186.270-26';
+SELECT cod_prato, nome_prato FROM Prato WHERE Prato.CPF = '165.186.270-26';
+
+--recuperar quais lotes um fornecedor provê
+--Versao com nome do ingrediente e data de entrega
+SELECT nome_fornecedor, Prove.cod_lote, nome_ingrediente, data_entrega FROM Fornecedor, Prove, Lote, Ingrediente WHERE Fornecedor.cnpj = Prove.cnpj AND Prove.cod_lote = Lote.cod_lote AND Lote.cod_ingrediente = Ingrediente.cod_ingrediente AND Fornecedor.cnpj = '83.358.102/0001-00';
+--Versao só com nome e cod
+SELECT nome_fornecedor, Prove.cod_lote FROM Fornecedor, Prove, Lote WHERE Fornecedor.cnpj = Prove.cnpj AND Prove.cod_lote = Lote.cod_lote AND Fornecedor.cnpj = '83.358.102/0001-00';
