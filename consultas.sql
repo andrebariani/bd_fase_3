a3bac2fdf8919ac6e3bc0abd0d4c28b1d7c4cf71
andrebariani <andrebariani@gmail.com>
2019-01-20 12:34:09 -0200

    consultas.sql v3

--- consultas.sql
+++ consultas.sql
@@ -2,13 +2,16 @@
 SELECT cod_lote AS Codigo, data_vencimento AS Vence_em FROM Ingrediente, Lote WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = '18';
 
 --Recuperar a quantidade total de um ingrediente
-SELECT Ingrediente.cod_ingrediente, SUM(quantidade_lote) FROM Lote, Ingrediente WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = '1';
+SELECT cod_ingrediente, SUM(quantidade_lote) FROM Lote, Ingrediente WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = '1' GROUP BY cod_ingrediente;
 
 --Recuperar a media dos preços dos pratos que cada cozinheiro faz
-SELECT nome_cozinheiro, AVG(preco_prato) AS media_preco_prato FROM Prato, Cozinheiro GROUP BY nome_cozinheiro;
+SELECT nome_cozinheiro, AVG(preco_prato) AS media_preco_prato FROM Prato, Cozinheiro WHERE Prato.CPF = Cozinheiro.CPF GROUP BY nome_cozinheiro;
 
---Recuperar Quais pratos determinado cozinheiro faz
-SELECT cod_prato, nome_prato FROM Prato WHERE Prato.CPF = '165.186.270-26';
+--Recuperar os pratos feitos por cada cozinheiro
+SELECT nome_cozinheiro, nome_prato FROM Prato, Cozinheiro WHERE Prato.CPF = Cozinheiro.CPF;
+
+--Recuperar receita de determinado prato
+SELECT nome_prato, nome_ingrediente, quantidade_usada FROM Prato, Ingrediente, Usa WHERE Prato.cod_prato = Usa.cod_prato AND Ingrediente.cod_ingrediente = Usa.cod_ingrediente AND nome_prato = 'Bolo de chocolate';
 
 --recuperar quais lotes um fornecedor provê
 --Versao com nome do ingrediente e data de entrega
