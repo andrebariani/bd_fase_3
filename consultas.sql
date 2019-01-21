cfe1da9a8295a7740ae7ca8f85c5a503ea57884d
andrebariani <andrebariani@gmail.com>
2019-01-21 13:27:05 -0200

    consultas.sql v8

--- consultas.sql
+++ consultas.sql
@@ -2,13 +2,13 @@
 
 --recuperar a data de validade de cada lote de certo ingrediente
 -- Autor: Giovani
--- Exemplo de <cod_ingrediente> = '18'
-SELECT cod_lote AS Codigo, data_vencimento AS Vence_em FROM Ingrediente, Lote WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = '18';
+-- Exemplo de <codingrediente> = '18'
+SELECT cod_lote AS Codigo, data_vencimento AS Vence_em FROM Ingrediente, Lote WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = <codingrediente>;
 
 --Recuperar a quantidade total de um ingrediente
 -- Autor: Giovani
--- Exemplo de <cod_ingrediente> = '4'
-SELECT Ingrediente.cod_ingrediente, SUM(quantidade_lote) FROM Lote, Ingrediente WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = '4' GROUP BY Ingrediente.cod_ingrediente;
+-- Exemplo de <codingrediente> = '4'
+SELECT Ingrediente.cod_ingrediente, SUM(quantidade_lote) FROM Lote, Ingrediente WHERE Ingrediente.cod_ingrediente = Lote.cod_ingrediente AND Ingrediente.cod_ingrediente = <codingrediente> GROUP BY Ingrediente.cod_ingrediente;
 
 --Recuperar a media dos preços dos pratos que cada cozinheiro faz
 -- Autor: André
@@ -16,18 +16,18 @@ SELECT nome_cozinheiro, AVG(preco_prato) AS media_preco_prato FROM Prato, Cozinh
 
 --Recuperar receita (ingrediente e quantidade) de determinado prato
 -- Autor: André
--- Exemplo de <cod_prato> = '8'
-SELECT nome_ingrediente, quantidade_usada FROM Ingrediente, Usa WHERE cod_prato = '8' AND Ingrediente.cod_ingrediente = Usa.cod_ingrediente;
+-- Exemplo de <codprato> = '8'
+SELECT nome_ingrediente, quantidade_usada FROM Ingrediente, Usa WHERE cod_prato = <codprato> AND Ingrediente.cod_ingrediente = Usa.cod_ingrediente;
 
 --recuperar quais lotes um fornecedor provê
 -- Autor: Mariane
--- Exemplo de <CNPJ> = '17.274.942/0001-60'
-SELECT nome_fornecedor, Prove.cod_lote FROM Fornecedor, Prove WHERE Fornecedor.cnpj = Prove.cnpj AND Fornecedor.cnpj = '17.274.942/0001-60';
+-- Exemplo de <cnpj> = '17.274.942/0001-60'
+SELECT nome_fornecedor, Prove.cod_lote FROM Fornecedor, Prove WHERE Fornecedor.cnpj = Prove.cnpj AND Fornecedor.cnpj = <cnpj>;
 
 --Recuperar os números de telefone de um fornecedor
 -- Autor: Mariane
--- Exemplo de <CNPJ> = '83.358.102/0001-00'
-select telefone_fornecedor from telefone,  fornecedor where telefone.cnpj = fornecedor.cnpj AND fornecedor.cnpj = '83.358.102/0001-00';
+-- Exemplo de <cnpj> = '83.358.102/0001-00'
+select telefone_fornecedor from telefone,  fornecedor where telefone.cnpj = fornecedor.cnpj AND fornecedor.cnpj = <cnpj>;
 
 -- Implementação da Trigger
 CREATE OR REPLACE FUNCTION calcula_salario() RETURNS trigger AS $calcula_salario$
